d2670bfd8abc863b31bbfc4c8995254219b881dd
William Casarin <jb55@jb55.com>
2019-09-26 18:21:19 -0700

    nix: fix build.rs
    Signed-off-by: William Casarin <jb55@jb55.com>

--- build.rs
+++ build.rs
@@ -5,6 +5,5 @@ fn main() {
     let wally_dir_str =
         wally_dir_path.as_path().to_str().expect("Please build wally first (in ./bld)");
     println!("cargo:rustc-link-search=native={}", wally_dir_str);
-    println!("cargo:rustc-link-lib=dylib=wallycore");
-    println!("cargo:rustc-link-lib=dylib=secp256k1");
+    println!("cargo:rustc-link-lib=static=wallycore");
 }
